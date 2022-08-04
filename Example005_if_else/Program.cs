7751b56dd23f4551d68f41783be24b18635d96cb
AnnaZimina <xadea@yandex.ru>
2022-08-04 16:16:50 +0300

    Example 005 If-Else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Who are you? ");
+string user = Console.ReadLine();
+if (user.ToLower() == "anna")
+{
+    Console.Write("Yeah! This is Anna!");
+}
+else
+{
+     Console.Write("Hello, ");
+    Console.WriteLine(user);
+}
