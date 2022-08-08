f9cadb080037963cc139273118da3798db711a18
AnnaZimina <xadea@yandex.ru>
2022-08-08 12:11:56 +0300

    Example 010 Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿//показать индекс искомого числа
+
+int[] array = {1, 22, 95, 56, 33, 47, 77, 95, 18};
+
+int n = array.Length;
+int find = 95;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+};
