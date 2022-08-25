6d4d50c2243117e3a69924cf742ca10be8aebb0b
AnnaZimina <xadea@yandex.ru>
2022-08-25 15:53:29 +0300

    Factorial & Fibinacci

--- Example015_Factorial/Program.cs
+++ Example015_Factorial/Program.cs
@@ -1,6 +1,16 @@
-﻿double Factorial (int n)
+﻿// factorial
+double Factorial(int n)
 {
-      if(n==1) return 1;
-      else return n*Factorial(n-1);
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
 }
-for (int i=1; i< 40; i++) Console.WriteLine($"{i}! = {Factorial(i)}");
+for (int i = 1; i < 40; i++) Console.WriteLine($"{i}! = {Factorial(i)}");
+
+// fibonacci f(1)=1, f(2)=1, f(n)=f(n-1)+f(n-2)
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++) Console.WriteLine($"f({i}) = {Fibonacci(i)}");
